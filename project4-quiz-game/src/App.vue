022dc1483eed44ca985d99d78b99fe7254332f89
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2023-09-22 15:36:33 -0300

    project 4

--- project4-quiz-game/src/App.vue
+++ project4-quiz-game/src/App.vue
@@ -1,17 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+    <template v-if="this.question">
+
+      <h1 v-html="this.question">
+      </h1>
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input type="radio" name="options" :value="answer" v-model="this.chosen_answer">
+        <label v-html="answer"></label><br>
+      </template>
+
+      <button @click="this.submitAnswer()" class="send" type="button">Send</button>
+    </template>
 </template>
+      
+
+  
+    
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosen_answer: undefined,
+    }
+  },
+
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      return answers;
+    }
+  },
+  methods:{
+    submitAnswer(){
+      if(!this.chosen_answer){
+        alert('Escolhe uma opção ai');
+      }else{
+        if(this.chosen_answer == this.correctAnswer){
+          alert('You got it')
+        }else{
+          alert('You lose')
+        }
+      }
+    }
+
+  },
+
+  created() {
+    this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=31&difficulty=hard&type=multiple')
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+
+      })
   }
 }
+
+//https://opentdb.com/api.php?amount=10&category=31&difficulty=hard&type=multiple
+
 </script>
 
 <style lang="scss">
@@ -21,6 +78,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
