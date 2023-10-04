9403bacfaa2ddf21496e8fe4ddba834b64aca0e7
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2023-10-04 16:18:13 -0300

    update 1

--- book-list/src/App.vue
+++ book-list/src/App.vue
@@ -1,19 +1,47 @@
 <script setup>
-import {ref} from 'vue';
+import { ref, reactive } from 'vue';
 
 let count = ref(0);
+
+let courses = reactive([{
+  title: "Javascript"
+}, {
+  title: "React"
+}, {
+  title: "Vue"
+},
+]);
+
+let newCourse = {}
+
+function increment() {
+  count.value++;
+}
+
+function addCourse() {
+  courses.push(newCourse);
+  newCourse = {};
+}
 
 </script>
 
 <template>
   <div>
 
-    <h1>{{ count }}</h1>
-    <button @click="count++">Incrementar</button>
+    <h1>Lista de Cursos</h1>
+    <ul>
+      <li v-for="course in courses" :key="course.title">
+        {{ course.title }}
+
+      </li>
+    </ul>
+
+    <br>
+
+    <input v-model="newCourse.title" type="text">
+    <button @click="addCourse">Adicionar</button>
 
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
