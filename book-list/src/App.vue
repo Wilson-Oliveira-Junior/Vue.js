76ba06d4c0c6468984bfb620521a6f613785d608
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2023-10-06 12:15:16 -0300

    final project

--- book-list/src/App.vue
+++ book-list/src/App.vue
@@ -1,47 +1,94 @@
 <script setup>
-import { reactive } from 'vue';
-import Progress from './components/Progress.vue'
+import {ref, reactive} from 'vue';
+import Books from './components/Books.vue';
+import BookProgress from './components/BookProgress.vue';
+import AddBook from './components/AdBook.vue';
 
-let courses = reactive([{
-  title: "Javascript",
-  done: true
-}, {
-  title: "React",
-  done: false
-}, {
-  title: "Vue",
-  done: true
-},
+let books = reactive([
+      {
+        id: 1,
+        title: "History of Europe",
+        cover:
+          "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg",
+        isRead: true,
+        isbn: "0-395-07157-8",
+        author: "Daniel Trejo",
+      },
+      {
+        id: 2,
+        title: "Penguin Classics",
+        cover:
+          "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-2-540x861.jpg",
+        isRead: false,
+        isbn: "0-395-07157-8",
+        author: "Daniel Trejo, Jon Snow",
+      },
+      {
+        id: 3,
+        title: "Becoming",
+        cover:
+          "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg",
+        isRead: false,
+        isbn: "0-395-07157-8",
+        author: "Daniel Trejo",
+      },
+      {
+        id: 4,
+        title: "Sonnets",
+        cover:
+          "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-5-540x861.jpg",
+        isRead: false,
+        isbn: "0-395-07157-8",
+        author: "Daniel Trejo",
+      },
 ]);
 
-let newCourse = {done: false}
 
-function addCourse() {
-  courses.push(newCourse);
-  newCourse = {done: false};
+function addBook(newBook) {
+  newBook.id = Math.max(...books.map(el => el.id)) + 1;
+  books.push(newBook);
+  showAddBook.value = false;
+}
+
+let showAddBook = ref(false);
+
+function toggleIsRead(id) {
+  books.forEach((book) => {
+    if (book.id === id) {
+      book.isRead = !book.isRead;
+    }
+  });
 }
 
 </script>
 
 <template>
-  <div>
+  
+  <div v-if="!showAddBook" class="container">
+    <h1>📖 Meus Livros</h1>
+    <div class="header-btns">
+      <button
+        class="btn"
+        @click="showAddBook = true"
+      >
+        Adicionar Livro +
+      </button>
+    </div>
+ 
+    <div class="books-container">
+      
+      <Books @toggleIsRead="toggleIsRead" :books="books" />
 
-    <h1>Lista de Cursos</h1>
-    <ul>
-      <li v-for="course in courses" :key="course.title">
-        {{ course.title }}
-
-      </li>
-    </ul>
-
-    <br>
-
-    <input v-model="newCourse.title" type="text">
-    <button @click="addCourse">Adicionar</button>
-
-    <Progress :courses="courses" />
-
+      <BookProgress :books="books"/>
+      
+    </div>
   </div>
+  <div v-else class="container">
+    <AddBook @addBook="addBook" @closeAddBook="showAddBook = false" />
+  </div>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
